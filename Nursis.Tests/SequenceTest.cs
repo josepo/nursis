1b5953efe6b29f1779ad28ff3f751f660376eafd
josepo <jlpolo@gmail.com>
2025-01-26 09:52:25 +0100

    implement get shifts

--- Nursis.Tests/SequenceTest.cs
+++ Nursis.Tests/SequenceTest.cs
@@ -14,4 +14,22 @@ public class SequenceTest
 
         Assert.NotNull(sequence);
     }
+
+    [Fact]
+    public void GetShifts()
+    {
+        Sequence sequence = new(
+        [
+            new ShiftPair(first: Shift.Early, second: Shift.Night),
+            new ShiftPair(first: Shift.None, second: Shift.None),
+            new ShiftPair(first: Shift.None, second: Shift.Late)
+        ]);
+
+
+        List<ShiftPair> shifts = sequence.GetShifs(start: 2, number: 2).ToList();
+
+        Assert.Equal(2, shifts.Count);
+        Assert.Equal(Shift.Late, shifts.First().Second);
+        Assert.Equal(Shift.Early, shifts.Last().First);
+    }
 }
--- Nursis/Sequence.cs
+++ Nursis/Sequence.cs
@@ -8,4 +8,17 @@ public class Sequence
     {
         _shifts = shifts;
     }
+
+    public IEnumerable<ShiftPair> GetShifs(int start, int number)
+    {
+        int returnedShifts = 0;
+        int totalNumberOfShifts = _shifts.Count;
+
+        for (int i = start % totalNumberOfShifts; returnedShifts < number; i = (i + 1) % totalNumberOfShifts)
+        {
+            yield return _shifts.ElementAt(i);
+
+            returnedShifts++;
+        }
+    }
 }
